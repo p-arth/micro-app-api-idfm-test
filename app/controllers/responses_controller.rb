680322130ecd0f756cdf6a03bb3b9d468f93ff9a
Pedro Arthur <pedro.art.vel@gmail.com>
2020-07-29 13:55:23 -0300

    API request working, getting response back properly

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,8 +1,6 @@
 require 'json'
-require 'open-uri'
 require 'pry'
 require 'rest-client'
-require 'net/http'
 require 'uri'
 require 'active_support/all'
 
@@ -22,26 +20,27 @@ class ResponsesController < ApplicationController
       client_secret: client_secret
     }
 
-    # uri = URI.parse('https://as.api.iledefrancemobilites.fr/api/oauth/token')
-    binding.pry
-
-
-
-    # http = Net::HTTP.new(uri.host, uri.port)
-    # http.use_ssl = true
-    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    # request = Net::HTTP::Post.new(uri.path)
-    # request.body = data.to_json
-
-    # result = http.request(request)
-
     result = RestClient.post( urlOAuth, data )
     result = JSON.parse(result)
 
+    token = result['access_token']
     binding.pry
 
+    apiUrl = 'https://traffic.api.iledefrance-mobilites.fr/v1/tr-global/estimated-timetable'
 
+    apiData = {
+      LineRef: 'ALL'
+    }
+
+    apiHeaders = {
+      'Accept-Encoding': '',
+      'Authorization': 'Bearer ' + token
+    }
+
+    finalResult = RestClient.get( apiUrl, headers = apiHeaders )
+    binding.pry
+    jsonData = JSON.parse(finalResult)
+    binding.pry
 
     @response = Response.new(response_params)
 
@@ -59,10 +58,10 @@ class ResponsesController < ApplicationController
 
   # POST /responses
   def create
-    binding.pry
-    urlOAuth = 'https://as.api.iledefrancemobilites.fr/api/oauth/token'
-    client_id = '4405c642-cba8-4e4f-8f29-12aabf069ae6'
-    client_secret = '519f9d77-8a40-4a8e-9a2a-5e26a83ec029'
+
+    urlOAuth = 'https://as.api.iledefrance-mobilites.fr/api/oauth/token'
+    client_id = ENV['CLIENT_ID']
+    client_secret = ENV['CLIENT_SECRET']
 
     data = {
       grant_type: 'client_credentials',
@@ -71,10 +70,11 @@ class ResponsesController < ApplicationController
       client_secret: client_secret
     }
 
-    result = RestClient.post(urlOAuth, data).to_json
+    result = RestClient.post( urlOAuth, data )
+    result = JSON.parse(result)
+
+    token = result['access_token']
     binding.pry
-
-
 
     @response = Response.new(response_params)
 
